93f24f47fbded6f2ac63a15fd03b9fd4eb388cbf
Nik O'Keefe <nikwillok@gmail.com>
2019-08-18 09:51:11 +1200

    added new gitignore

--- src/api.js
+++ src/api.js
@@ -3,11 +3,17 @@ import axios from 'axios';
 export const fetchContest = contestId => {
   return axios
     .get(`/api/contests/${contestId}`)
-    .then(response => response.data)
+    .then(response => response.data);
 };
 
 export const fetchContestList = () => {
   return axios
-    .get('/api/contests')
-    .then(response => response.data.contests)
+  .get('/api/contests')
+  .then(response => response.data.contests)
+};
+
+export const fetchNames = nameIds => {
+  return axios
+    .get(`/api/names/${nameIds.join(',')}`)
+    .then(response => response.data.names);
 };
